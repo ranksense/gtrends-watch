bfc6fcded2ccb916a8fa97605a524cea12e08e05
Hamlet Batista <hamletb@hamletbatista.com>
2019-05-13 09:16:20 -0400

    fixed static paths to work on server rendering

--- static.paths.ts
+++ static.paths.ts
@@ -1,2 +1,2 @@
 //added manually
-export const ROUTES = ["/dashboard", "/trends"]//, "/topics/"]
+export const ROUTES = ["/dashboard", "/trends", "/topics/1","/topics/2", "/topics/3","/topics/4", "/topics/5"]
